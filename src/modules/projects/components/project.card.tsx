04f81881a094bfeeff6f55c927fc3a3ae5730202
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-07-27 18:03:26 -0500

    feat(project): add description to project card

--- src/modules/projects/components/project.card.tsx
+++ src/modules/projects/components/project.card.tsx
@@ -9,7 +9,7 @@ interface ProjectCardProps {
 }
 
 export function ProjectCard({
-  project: { id, name, smImage },
+  project: { id, name, smImage, description },
 }: ProjectCardProps) {
   const { getProjectLink } = useMyRouter();
 
@@ -18,24 +18,23 @@ export function ProjectCard({
   return (
     <div className="card lg:card-side bg-base-200 shadow-xl mb-4 items-center">
       {smImage ? (
-        <div className="mockup-window bg-base-300 w-80 mx-8 mt-8 lg:mr-0 lg:mb-8">
+        <div className="mockup-window bg-base-300 w-80 mx-8 mt-8 lg:mr-0 lg:mb-8 shrink-0">
           <figure>
             <img src={`${FETCH_STATIC_DATA}${smImage}`} alt="Album" />
           </figure>
         </div>
       ) : null}
-      <div className="card-body">
-        <div className="flex flex-col lg:flex-row items-center">
-          <div className="flex-1 pr-0 md:pr-8">
-            <h3 className="card-title text-primary mb-4 font-medium text-center">
-              {name}
-            </h3>
-          </div>
-          <div className="flex-0 flex place-items-center">
-            <Link to={projectLink} className="btn btn-outline btn-primary">
-              Go to Project
-            </Link>
-          </div>
+      <div className="card-body md:flex-row items-center md:items-start">
+        <div className="flex-1 pr-0 md:pr-8">
+          <h3 className="card-title text-primary mb-4 font-medium text-center">
+            {name}
+          </h3>
+          <p className="hidden mb-8 md:block lg:mb-0">{description}</p>
+        </div>
+        <div className="flex-0 flex place-items-center ">
+          <Link to={projectLink} className="btn btn-outline btn-primary">
+            Go to Project
+          </Link>
         </div>
       </div>
     </div>
