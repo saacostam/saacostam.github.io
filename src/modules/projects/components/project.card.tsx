90e9320525fb35972924aeda9f62b7251139046e
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-07-27 17:25:25 -0500

    fix(project): render image correctly

--- src/modules/projects/components/project.card.tsx
+++ src/modules/projects/components/project.card.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import { Project } from "../project.type";
 import { useMyRouter } from "../../router";
+import { FETCH_STATIC_DATA } from "../../constants";
 
 interface ProjectCardProps {
   project: Project;
@@ -15,14 +16,16 @@ export function ProjectCard({
   const projectLink = getProjectLink(id);
 
   return (
-    <div className="card lg:card-side bg-base-200 shadow-xl mb-4">
+    <div className="card lg:card-side bg-base-200 shadow-xl mb-4 items-center">
       {smImage ? (
-        <figure>
-          <img src={smImage} alt="Album" />
-        </figure>
+        <div className="mockup-window bg-base-300 w-80 mx-8 mt-8 lg:mr-0 lg:mb-8">
+          <figure>
+            <img src={`${FETCH_STATIC_DATA}${smImage}`} alt="Album" />
+          </figure>
+        </div>
       ) : null}
       <div className="card-body">
-        <div className="flex flex-row">
+        <div className="flex flex-col lg:flex-row items-center">
           <div className="flex-1 pr-0 md:pr-8">
             <h3 className="card-title text-primary mb-4 font-medium">{name}</h3>
           </div>
