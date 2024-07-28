e160814a466b3dea6069f028b952c610a9dc7f82
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-07-27 22:32:52 -0500

    feat(project): add alert if iframe is project iframe is not responsive or mobile-friendly

--- src/modules/icons/icons/index.ts
+++ src/modules/icons/icons/index.ts
@@ -2,6 +2,7 @@ export * from "./bolt";
 export * from "./bars-3";
 export * from "./command-line";
 export * from "./device-phone-mobile";
+export * from "./info";
 export * from "./refresh";
 export * from "./search";
 export * from "./x";
--- src/modules/projects/components/project-content.tsx
+++ src/modules/projects/components/project-content.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../core";
+import { InfoIcon, XIcon } from "../../icons";
 import { Project } from "../project.type";
 
 export interface ProjectContentProps {
@@ -6,17 +8,53 @@ export interface ProjectContentProps {
 }
 
 export function ProjectContent({
-  project: { description, name, url, iframe },
+  project: { id, description, name, url, iframe },
 }: ProjectContentProps) {
   const height = iframe.height || 750;
   const width = iframe.width;
+
+  const { canBeUsedInMobile, isResponsive } = iframe;
+
+  const viewPortIssues = [
+    ...(canBeUsedInMobile === false ? ["No Mobile Controls"] : []),
+    ...(isResponsive === false ? ["Not Responsive"] : []),
+  ];
+
+  const [showViewPortIssuesAlert, setShowViewPortIssuesAlert] = useState(true);
+
+  useEffect(() => {
+    setShowViewPortIssuesAlert(true);
+  }, [id]);
 
   return (
     <section>
       <Header className="text-center mb-8" size="2xl" headerLevel={2}>
         {name}
       </Header>
-      <p className="mb-8">{description}</p>
+      <p className="mb-4">{description}</p>
+      {viewPortIssues.length > 0 && showViewPortIssuesAlert ? (
+        <div
+          className="lg:hidden relative alert alert-warning shadow-lg opacity-90 mb-4"
+          role="alert"
+        >
+          <InfoIcon className="hidden md:block" />
+          <div>
+            <h3 className="font-bold">Ooops!</h3>
+            <div className="text-xs">
+              This project may not work on smaller viewports:{" "}
+              <span className="font-semibold">
+                {viewPortIssues.join(" - ")}
+              </span>
+            </div>
+          </div>
+          <button
+            className="btn btn-sm btn-circle btn-warning absolute right-4 top-4"
+            onClick={() => setShowViewPortIssuesAlert(false)}
+          >
+            <XIcon />
+          </button>
+        </div>
+      ) : null}
       <div
         className="mockup-window bg-base-300 border mx-auto"
         style={{ ...(width ? { width: width } : {}) }}
