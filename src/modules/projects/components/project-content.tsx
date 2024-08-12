315e8eb4a04836c681b6c5bbb6aeb0712907a847
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-08-11 22:25:26 -0500

    feat(project): add link to repository

--- src/modules/projects/components/project-content.tsx
+++ src/modules/projects/components/project-content.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../core";
+import { FireIcon, GithubIcon } from "../../icons";
 import { Project } from "../types";
 import { useWindowDimensions } from "../../viewport";
 import { ViewportAlert } from "./viewport-alert";
@@ -8,7 +9,7 @@ export interface ProjectContentProps {
 }
 
 export function ProjectContent({
-  project: { id, description, name, url, iframe },
+  project: { id, description, name, url, iframe, repoUrl },
 }: ProjectContentProps) {
   const { width: windowWidth } = useWindowDimensions();
 
@@ -26,10 +27,26 @@ export function ProjectContent({
 
   return (
     <section key={id}>
-      <Header className="text-center mb-8" size="2xl" headerLevel={2}>
+      <Header className="text-center mb-4" size="2xl" headerLevel={2}>
         {name}
       </Header>
       <p className="mb-4">{description}</p>
+      <div className="flex flex-wrap mb-4">
+        <a
+          href={url}
+          target="_blank"
+          className="btn btn-secondary btn-outline btn-sm mr-4"
+        >
+          <FireIcon className="inline" /> Demo
+        </a>
+        <a
+          href={repoUrl}
+          target="_blank"
+          className="btn btn-outline btn-sm mr-4"
+        >
+          <GithubIcon className="inline" /> Code
+        </a>
+      </div>
       {viewPortIssues.length > 0 ? (
         <ViewportAlert viewPortIssues={viewPortIssues} />
       ) : null}
