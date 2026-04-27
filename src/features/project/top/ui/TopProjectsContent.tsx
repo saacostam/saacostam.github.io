d2df10f738a98a1cc97f7cab9e05145be5d94f58
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-27 12:11:16 -0500

    feat(project): add link to top projects items

--- src/features/project/top/ui/TopProjectsContent.tsx
+++ src/features/project/top/ui/TopProjectsContent.tsx
@@ -1,4 +1,5 @@
-import { Flex } from "@mantine/core";
+import { Flex, UnstyledButton } from "@mantine/core";
+import { Link } from "react-router";
 import type { IProject } from "@/features/project/core/domain";
 import { ProjectItem } from "@/features/project/core/ui";
 
@@ -10,7 +11,9 @@ export function TopProjectsContent({ topProjects }: TopProjectsContentProps) {
 	return (
 		<Flex direction="column" gap="xs">
 			{topProjects.map((project) => (
-				<ProjectItem key={project.id} project={project} />
+				<UnstyledButton key={project.id} component={Link} to={"#"}>
+					<ProjectItem key={project.id} project={project} />
+				</UnstyledButton>
 			))}
 		</Flex>
 	);
