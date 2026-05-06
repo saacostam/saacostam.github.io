8e101343f4bc1fe72a13d6161f966b8c8c756cb7
saacostam <saacostam@users.noreply.github.com>
2026-05-06 06:03:20 +0000

    deploy: bcb90ebed8319ffb7dcd1120beeae7595a22e211

--- assets/use-meta-query-95Bj8rRW.js
+++
@@ -1,1 +0,0 @@
-import{d as n,c as y}from"./ProjectItem-3vfC1e3R.js";import{N as i,r as c}from"./index-CziWTNOd.js";function o(r,e){return n(r,y)}function l(r){const{queryKey:e,queryFn:s}=r,t=i(),a=c.useCallback(u=>{e&&t.setQueryData(e,u)},[t,e]);return{useQuery:u=>o({...r,...u,queryKey:e,queryFn:s}),setOptimisticData:a}}export{l as u};
