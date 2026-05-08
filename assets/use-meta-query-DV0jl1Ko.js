d80191e76bfd344919a975916971015866d810cb
saacostam <saacostam@users.noreply.github.com>
2026-05-08 19:50:24 +0000

    deploy: 179c2657e1d314c3ee2ef92dd541edc6feae59a0

---
+++ assets/use-meta-query-DV0jl1Ko.js
@@ -0,0 +1,1 @@
+import{d as n,c as y}from"./ProjectItem-5rdlwuzE.js";import{N as i,r as c}from"./index-D-sTcV9x.js";function o(r,e){return n(r,y)}function l(r){const{queryKey:e,queryFn:s}=r,t=i(),a=c.useCallback(u=>{e&&t.setQueryData(e,u)},[t,e]);return{useQuery:u=>o({...r,...u,queryKey:e,queryFn:s}),setOptimisticData:a}}export{l as u};
