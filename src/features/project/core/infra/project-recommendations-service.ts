218461e91250f5209ce7591c7563d1737888dc41
saacostam <73088662+saacostam@users.noreply.github.com>
2026-05-08 14:35:53 -0500

    feat(project): remove category-based clustering

--- src/features/project/core/infra/project-recommendations-service.ts
+++ src/features/project/core/infra/project-recommendations-service.ts
@@ -1,18 +1,4 @@
-import {
-	type IProject,
-	IProjectCategory,
-} from "@/features/project/core/domain";
-
-interface IProjectWithDistance {
-	project: IProject;
-	distance: number;
-}
-
-const MAP_CATEGORY_TO_NUMBERED_PROPERTY = new Map<IProjectCategory, number>([
-	[IProjectCategory.MusicSoftware, 50],
-	[IProjectCategory.SoftwareEngineering, 30],
-	[IProjectCategory.Games, 10],
-]);
+import type { IProject } from "@/features/project/core/domain";
 
 export const projectRecommendationsService = {
 	findClosestNProjects(
@@ -24,49 +10,9 @@ export const projectRecommendationsService = {
 			(project) => project.id !== ref.id,
 		);
 
-		const projectWithDistances: IProjectWithDistance[] = differentProjects.map(
-			(project): IProjectWithDistance => ({
-				project: project,
-				distance: euclideanDistance(
-					this.mapProjectToPropertiesNumberVector(ref),
-					this.mapProjectToPropertiesNumberVector(project),
-				),
-			}),
-		);
+		const ascendingOrderingCriterion = (a: IProject, b: IProject): number =>
+			a.rating - b.rating;
 
-		const ascendingOrderingCriterion = (
-			a: IProjectWithDistance,
-			b: IProjectWithDistance,
-		): number =>
-			0.5 * (a.distance - b.distance) +
-			0.5 * (b.project.rating - a.project.rating);
-
-		return projectWithDistances
-			.sort(ascendingOrderingCriterion)
-			.slice(0, n)
-			.sort((a, b) => b.project.rating - a.project.rating)
-			.map(({ project }) => project);
-	},
-
-	mapProjectToPropertiesNumberVector(proj: IProject): number[] {
-		const projectCategoryValue = proj.category.reduce((value, category) => {
-			const categoryValue = MAP_CATEGORY_TO_NUMBERED_PROPERTY.get(category);
-
-			return value + (categoryValue ?? 0);
-		}, 0);
-
-		return [projectCategoryValue];
+		return differentProjects.sort(ascendingOrderingCriterion).slice(0, n);
 	},
 };
-
-function _squaredEuclidean(p: number[], q: number[]): number {
-	let d: number = 0;
-	for (let i = 0; i < p.length; i++) {
-		d += (p[i] - q[i]) * (p[i] - q[i]);
-	}
-	return d;
-}
-
-function euclideanDistance(p: number[], q: number[]): number {
-	return Math.sqrt(_squaredEuclidean(p, q));
-}
